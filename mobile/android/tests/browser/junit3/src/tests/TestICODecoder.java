edd9702f534ecaef8279d36beee2ebf6375ef4d7
Franz Sarmiento <franzks@gmail.com>
2014-06-29 12:28:51 +0800

    Created empty TestICODecoder
    This is for unit testing of the ICODecoder object to test how it
    handles ico files with varying properties such as size and color
    depth.

---
+++ mobile/android/tests/browser/junit3/src/tests/TestICODecoder.java
@@ -0,0 +1,14 @@
+/* Any copyright is dedicated to the Public Domain.
+   http://creativecommons.org/publicdomain/zero/1.0/ */
+
+package org.mozilla.gecko.browser.tests;
+
+/**
+ * A test to validate how ICODecoder handles ico files with varying
+ * properties such as size and color depth.
+ */
+public class TestICODecoder extends BrowserTestCase {
+    public void testICODecoder() {
+        
+    }
+}
